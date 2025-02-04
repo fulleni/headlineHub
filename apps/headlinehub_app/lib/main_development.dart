0900bb1f69ca3cbaae4b52eef628e21c9ef3d36d
fulleni <fulleni@hotmail.com>
2025-02-04 12:25:08 +0100

    chore: added gihub workflows

--- apps/headlinehub_app/lib/main_development.dart
+++ apps/headlinehub_app/lib/main_development.dart
@@ -1,7 +1,8 @@
 import 'package:headlinehub_app/app/app.dart';
 import 'package:headlinehub_app/bootstrap.dart';
+import 'package:headlineshub_headlines_client/headlineshub_headlines_client.dart';
 
 void main() {
-  final headlinesClint = Headheacl
+  final headlinesClint = HeadlinehubHeadlinesClient(baseUrl, httpClient)
   bootstrap(() => const App());
 }
